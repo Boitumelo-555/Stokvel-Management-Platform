18b38c2668121c9e9746954687d4219298e1944e
Fhatu57 <www.tedmusekwa57.com@gmail.com>
2026-04-11 23:38:26 +0200

    feat : Landing page where a member first opens the site TG-13

--- frontend/Landing.js
+++
@@ -1,9 +0,0 @@
-function toggleNav() {
-  const sidebar = document.getElementById("mySidebar");
-  
-  if (sidebar.style.width === "250px") {
-    sidebar.style.width = "0";
-  } else {
-    sidebar.style.width = "250px";
-  }
-}
